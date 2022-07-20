168f9b03f8e4eec9602f18099e47e1715cb770eb
N4choCM <104795448+N4choCM@users.noreply.github.com>
2022-07-20 21:09:56 +0200

    Add files via upload

---
+++ ChaptersServices.cs
@@ -0,0 +1,12 @@
+﻿using UniversityApiBackend.Models.DataModels;
+
+namespace UniversityApiBackend.Services
+{
+    public class ChaptersServices : IChaptersServices
+    {
+        public IEnumerable<Chapter> GetSpecificCourseChapter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IChaptersServices.cs
@@ -0,0 +1,9 @@
+﻿using UniversityApiBackend.Models.DataModels;
+
+namespace UniversityApiBackend.Services
+{
+    public interface IChaptersServices
+    {
+        IEnumerable<Chapter> GetSpecificCourseChapter();
+    }
+}
